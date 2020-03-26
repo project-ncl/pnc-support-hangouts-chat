cab66d0d7907635c6ff7d3853f5d8a9a9abde408
Dustin Kut Moy Cheung <dcheung@redhat.com>
2020-03-25 22:14:43 -0400

    Initial commit

---
+++ Code.js
@@ -0,0 +1,49 @@
+/**
+ * Responds to a MESSAGE event in Hangouts Chat.
+ *
+ * @param {Object} event the event object from Hangouts Chat
+ */
+function onMessage(event) {
+  var name = "";
+
+  if (event.space.type == "DM") {
+    name = "You";
+  } else {
+    name = event.user.displayName;
+  }
+  var message = name + " said \"" + event.message.text + "\"";
+
+  return { "text": message };
+}
+
+/**
+ * Responds to an ADDED_TO_SPACE event in Hangouts Chat.
+ *
+ * @param {Object} event the event object from Hangouts Chat
+ */
+function onAddToSpace(event) {
+  var message = "";
+
+  if (event.space.type == "DM") {
+    message = "Thank you for adding me to a DM, " + event.user.displayName + "!";
+  } else {
+    message = "Thank you for adding me to " + event.space.displayName;
+  }
+
+  if (event.message) {
+    // Bot added through @mention.
+    message = message + " and you said: \"" + event.message.text + "\"";
+  }
+
+  return { "text": message };
+}
+
+/**
+ * Responds to a REMOVED_FROM_SPACE event in Hangouts Chat.
+ *
+ * @param {Object} event the event object from Hangouts Chat
+ */
+function onRemoveFromSpace(event) {
+  console.info("Bot removed from ", event.space.name);
+}
+
